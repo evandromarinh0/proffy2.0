e271a3e7e0df10af03f96f975bff51162562bb06
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-01-13 15:55:01 -0300

    input styles ok

--- appproffy/src/components/Input/styles.ts
+++ appproffy/src/components/Input/styles.ts
@@ -1,13 +1,24 @@
+import { css } from 'styled-components';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
+  flex-direction: row;
   height: 64px;
   background: #fafafc;
   border: 1px solid #E6E6F0;
   border-radius: 8px;
   padding: 0 16px;
   justify-content: center;
+
+  ${props => props.isFocused && css`
+    border-color: #8557e5;
+  `}
+  border-width: 2px;
 `;
 
 export const TextInput = styled.TextInput`
