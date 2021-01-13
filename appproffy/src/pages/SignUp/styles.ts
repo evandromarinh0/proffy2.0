681df46550bbe7108220bdcc015acb14eaf54690
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-01-13 15:57:53 -0300

    signup page styles ok

--- appproffy/src/pages/SignUp/styles.ts
+++ appproffy/src/pages/SignUp/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components/native';
 
+export const BackButton = styled.TouchableOpacity`
+  padding: 24px;
+`;
+
 export const Container = styled.View`
-  
+  flex: 1;
+  justify-content: center; 
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  color: #32264d;
+  font-size: 24px;
+  font-family: 'Poppins-SemiBold';
+  align-self: flex-start;
 `;
