4a31b8079e24dc6c559c5aa2b87247844dd9e9a5
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-01-18 17:53:16 -0300

    validation fields done

--- appproffy/src/pages/SignUp/index.tsx
+++ appproffy/src/pages/SignUp/index.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Image, KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
+import { Alert, Image, KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 import backImg from '../../assets/arrow-left.png';
 import Button from '../../components/Button';
@@ -14,6 +15,17 @@ import {
   BackButton,
   Title,
 } from './styles';
+import getValidationErrors from '../../utils/ValidationError';
+
+interface SignUpNameForm {
+  firstName: string;
+  lastName: string;
+}
+
+interface SignUpCredentialsForm {
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
@@ -25,19 +37,78 @@ const SignUp: React.FC = () => {
     navigation.goBack();
   }, [navigation]);
 
-  const handleSignUpFullName = useCallback((data: object) => {
-    console.log(data);
+  const handleSignUpFullName = useCallback(async (data: SignUpNameForm) => {
+    try {
+      formIdRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        firstName: Yup.string().required('Nome obrigatório'),
+        lastName: Yup.string().required('Sobrenome obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // the request to api should be done here, example:
+
+      // await api.post('users', data);
+
+      swiperRef.current.scrollBy(1);
+
+    } catch (err) {
+      if(err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formIdRef.current?.setErrors(errors);
+
+        return;
+      }
+    }
   }, []);
 
   const handleSwipeNext = useCallback(() => {
     formIdRef.current?.submitForm();
-
-    swiperRef.current.scrollBy(1);
   }, []);
 
-  const handleSignUpCredentials = useCallback((data: object) => {
-    console.log(data)
-  }, []);
+  const handleSignUpCredentials = useCallback(async (data: SignUpCredentialsForm) => {
+    try {
+      formCredentialsRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('Campo obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Campo obrigatório')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // the request to api should be also done here, example:
+
+      // await api.post('users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Seu cadastro foi realizado, você já pode fazer o seu login'
+      )
+
+      navigation.navigate('SignIn');
+    } catch (err) {
+      if(err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formCredentialsRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer cadastro, tente novamente'
+      )
+    }
+  }, [navigation]);
 
   return (
     <>
@@ -56,18 +127,18 @@ const SignUp: React.FC = () => {
             <Title>01. Quem é você?</Title>
             <Form ref={formIdRef} onSubmit={handleSignUpFullName}>
               <Input 
-                name="first-name" 
+                name="firstName" 
                 autoCorrect={true}
                 autoCapitalize="words"
                 placeholder="Nome" 
               />
               <Input 
-                name="last-name" 
+                name="lastName" 
                 autoCorrect={true}
                 autoCapitalize="words"
                 placeholder="Sobrenome" 
               />
-              <Button color onPress={handleSwipeNext}>Próximo</Button>
+              <Button custom onPress={handleSwipeNext}>Próximo</Button>
             </Form>
           </Container>
         </KeyboardAvoidingView>
